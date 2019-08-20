c25c4a61361f9e7810a161f4ecfdab0a7bc4616b
System Administrator <root@E-fly-MBP.localdomain>
2019-08-20 13:14:19 +0100

    Updates

---
+++ precache-manifest.2d1c09c63eb0ed73890b901f5b6ae5ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe82143d7695703ab13b63fad61cadcc",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "00a8242c0f3f2532b14a",
+    "url": "/Portfolio/static/css/main.771bd80b.chunk.css"
+  },
+  {
+    "revision": "f80f0acbd2d0ce47e57c",
+    "url": "/Portfolio/static/js/2.917ae229.chunk.js"
+  },
+  {
+    "revision": "00a8242c0f3f2532b14a",
+    "url": "/Portfolio/static/js/main.b76b191e.chunk.js"
+  },
+  {
+    "revision": "9534e064c61335e7c934",
+    "url": "/Portfolio/static/js/runtime~main.3a3ea873.js"
+  },
+  {
+    "revision": "2833cae0c987ff7de1af1a4853dd5abf",
+    "url": "/Portfolio/static/media/background.2833cae0.png"
+  }
+]);
