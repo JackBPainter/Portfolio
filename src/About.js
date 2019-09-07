7faffeb84c95cbae77bc402a181bded9dc3bfdc1
Jack Painter <littlesympathy@jacks-mbp.home>
2019-09-07 16:31:20 +0100

    Create project page and improve app consistency

--- src/About.js
+++ src/About.js
@@ -4,23 +4,23 @@ import Face from "./mainImages/myFace.png";
 export default function About() {
   return (
     <div className="About-Container">
-      <div className="Float-Me">
+      <div className="Float-Me-About">
         <img className="face" src={Face} alt="My Face"/>
       </div>
       <h2 className="About-header">About Me</h2>
-      <p className="About-item">
-        My name is Jack Painter and I am a enthusiastic and passionate junior
+      <p className="About-writing">
+        Hey I'm Jack, an enthusiastic and passionate junior
         full-stack developer. After more than 4 years of working on the sales
         side of IT, I decided enough was enough and made the decision to drop it
         all and become a developer.
         <br />
         <br />
-        Currently on an intensive full-stack development course with Kodiri
+        Currently I'm on an intensive full-stack development course with Kodiri
         where I am using JavaScript, React, Node, HTML and CSS to develop a
-        netflix clone and a soon to be disclosed group project.
+        Netflix clone alongside a soon to be disclosed group project.
         <br />
         <br />
-        When I'm not coding your find me taking photos(any tips would be very
+        When I'm not coding you'll find me playing about with my DSLR (any tips would be very
         welcome), gaming, listening to vinyls, attending meetups and following
         the Formula 1 & football.
       </p>
--- src/Projects.js
+++ src/Projects.js
@@ -1,11 +1,24 @@
 import React from "react";
 
 export default function Projects() {
-    return (
-        <div className="Projects-Page">
-            <div className="Projects-Container">
-                <h2 className="Projects-Header">My Projects</h2>
-            </div>
-        </div>
-    )
+  return (
+    <div className="Projects-Page">
+      <h2 className="Projects-Header">My Projects</h2>
+      <div className="Projects-Icon-Container">
+          <div className="Float-Me-Projects">
+          </div>
+          <br />
+          <p className="Projects-Text">Kodflix</p>
+          <br />
+          <div className="Float-Me-Projects"></div>
+          <br />
+          <p className="Projects-Text">Bills/Electricity Calculator</p>
+          <br />
+          <div className="Float-Me-Projects"></div>
+          <br />
+          <p className="Projects-Text">progrAmateur</p>
+          <br />
+      </div>
+    </div>
+  );
 }
