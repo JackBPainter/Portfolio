ed87f81ff4b19b655ed2880ebd747dda62cce4e6
Jack Painter <littlesympathy@jacks-mbp.home>
2019-09-14 15:07:00 +0100

    add email link to nav bar

--- src/Nav-Bar.js
+++ src/Nav-Bar.js
@@ -1,14 +1,22 @@
 import React from "react";
 import LinkedIn from "./mainImages/LinkedIn.svg";
 import GitHub from "./mainImages/GitHub.svg";
+import Email from "./mainImages/EmailIcon.png";
 
 export default function NavBar() {
   return (
     <div className="Nav-Bar">
       <nav>
         <ul className="Nav-Bar-Container">
-          <img className="Social-Icon" src={LinkedIn} alt="LinkedIn Link" />
-          <img className="Social-Icon" src={GitHub} alt="Github Link" />
+          <a href="https://linkedin.com/in/painterjack/">
+            <img className="Social-Icon" src={LinkedIn} alt="LinkedIn Link" />
+          </a>
+          <a href="https://github.com/JackBPainter">
+            <img className="Social-Icon" src={GitHub} alt="Github Link" />
+          </a>
+          <a href="mailto:JackBPainter@gmail.com">
+            <img className="Social-Icon" src={Email} alt="Email Link" />
+          </a>
           <li>
             <a href="#Contact">Contact</a>
           </li>
