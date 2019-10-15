b9c11e27b6c7dad1a69bff7ce9f21290a22ee1f8
Jack Painter <littlesympathy@Jacks-MacBook-Pro.local>
2019-10-15 13:01:53 +0100

    Add Tetris project and make the project images flexible

--- src/Projects.js
+++ src/Projects.js
@@ -1,7 +1,8 @@
 import React from "react";
 import Kodflix from "./mainImages/Kodflix-Full.png";
-import Calculator from "./mainImages/Calculator-Full.png"
-import progrAmateur from "./mainImages/ProgramateurFinal.png"
+import Calculator from "./mainImages/Calculator-Full.png";
+import progrAmateur from "./mainImages/ProgramateurFinal.png";
+import Tetris from "./mainImages/Tetris.png";
 
 export default function Projects() {
   return (
@@ -14,17 +15,26 @@ export default function Projects() {
           <br />
           <p className="Projects-Text">Kodflix<br />(Netflix Clone)<br /><a className="Heroku-Links" href="https://jpkodflix.herokuapp.com/">Deployed App</a></p>
           <br />
+          <br />
           <a className="Float-Me-Projects" href="https://github.com/JackBPainter/shine-energy-calculator">
             <img className="Calculator" src={Calculator} alt="Calculator"/>
           </a>
           <br />
           <p className="Projects-Text">Gas/Electricity Calculator</p>
           <br />
+          <br />
           <a className="Float-Me-Projects" href="https://github.com/JackBPainter/progrAmateur">
               <img className="progrAmateur" src={progrAmateur} alt="progrAmateur" />
           </a>
           <br />
           <p className="Projects-Text">progrAmateur<br />(Junior Developer messaging application)<br /><a className="Heroku-Links" href="http://programateur.herokuapp.com/">Deployed App</a></p>
+          <br />
+          <br />
+          <a className="Float-Me-Projects" href="https://github.com/JackBPainter/Tetris">
+              <img className="Tetris" src={Tetris} alt="Tetris" />
+          </a>
+          <p className="Projects-Text">Tetris</p>
+          <br />
           <br />
       </div>
     </div>
